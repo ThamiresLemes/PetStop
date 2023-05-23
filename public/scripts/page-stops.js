ff9d34c1a297a9253f9c9753bc51b93cec689807
Thamires Lemes <thamireslemesle@gmail.com>
2023-05-23 19:10:06 -0300

    finalizacao da pagina stops com modificacao de imagens

--- public/scripts/page-stops.js
+++ public/scripts/page-stops.js
@@ -1,6 +1,6 @@
 // create map
 
-const map = L.map("mapid").setView([-22.9183902,-42.8313887], 15);
+const map = L.map("mapid").setView([-22.9183902, -42.8313887], 16);
 
 // create and add TileLayer
 
@@ -8,7 +8,27 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // create icon
 
-L.marker([-22.9183902,-42.8313887])
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+// create popup overlay
+
+const popup = L.popup({
+  closeButton: false,
+  className: "map-popup",
+  minWidth: 240,
+  minHeight: 240,
+}).setContent(
+  'Ponto de Hidratação e Alimentação <a href="stop.html?id=1" class="choose-stop"> <img src="./public/images/arrow-white.svg"> </a>'
+);
+
+// create and add Maker
+
+L.marker([-22.9183902, -42.8313887], { icon })
   .addTo(map)
-  .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+  .bindPopup(popup)
   .openPopup();
